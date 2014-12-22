ac5ef087f49682734ca7c731319cd5220a5a404a
xssChauhan <shikhar.chauhan@live.com>
2014-12-22 06:16:58 +0530

    Implemented heapify, deletemin, building the heap and inserting in heap

--- Python/minheap.py
+++ Python/minheap.py
@@ -38,7 +38,48 @@ class minheap:
         return right
 
     #Heapify the heap
-    def heapify(self):
+    def heapify(self, i):
+
+        l = self.leftchild(i)
+        r = self.rightchild(i)
+        min = i
+        if( l <  self.heaplength() and  self.heap[min] > self.heap[l] ):
+            min = l;
+        if( r < self.heaplength() and self.heap[min] > self.heap[r] ):
+            min = r;
+
+        if( min != i):
+            """Swap the element with i"""
+            self.heap[i] , self.heap[min] = self.heap[min] , self.heap[i]
+
+        self.heapify(min)
+
+    #Inserting element into the heap
+    def insert(self, data):
+        self.heap.append(data)
+        i = self.heaplength()
+        self.heapify(i)
+
+    #Building the heap
+    def buildheap(self , nums):
+        for i in xrange(nums):
+            self.heap[i] = input()
+
+    #Deleting the topmost element of the heap, which in this case is the minimum element of the heap
+    def deletemin(self):
+        if( self.heaplength() == 0 )
+            return -1
+        data = self.heap[0]
+        self.heap = self.heap[1:]
+        self.heapify(self.heaplength())
+        return data
+    
+
+
+
+
+
+
 
 
 
